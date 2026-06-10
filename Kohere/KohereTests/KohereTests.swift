2189968bede885dbb5366f1993cae12c0febfc9e
SongKyuSeob <sks5075@naver.com>
2026-06-11 02:48:53 +0900

    setting: 프로젝트 생성

---
+++ Kohere/KohereTests/KohereTests.swift
@@ -0,0 +1,17 @@
+//
+//  KohereTests.swift
+//  KohereTests
+//
+//  Created by 송규섭 on 6/11/26.
+//
+
+import Testing
+@testable import Kohere
+
+struct KohereTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
