ed8e33a0370d5c7fb59cd588314fd448bd1750a8
SongKyuSeob <sks5075@naver.com>
2026-06-20 18:34:38 +0900

    setting: UIDesignRequiresCompatibility 설정 제거

--- Kohere/Kohere/KohereApp.swift
+++ Kohere/Kohere/KohereApp.swift
@@ -12,8 +12,7 @@ import UIKit
 @main
 struct KohereApp: App {
     init() {
-        UITabBar.appearance().tintColor = UIColor(named: "primary50")
-        UITabBar.appearance().unselectedItemTintColor = UIColor(named: "coolNeutral50")
+        configureTabBarAppearance()
     }
 
     var body: some Scene {
@@ -24,5 +23,49 @@ struct KohereApp: App {
                 }
             )
         }
+    }
+
+    private func configureTabBarAppearance() {
+        let selectedColor = UIColor(named: "primary50")
+        let normalColor = UIColor(named: "coolNeutral50")
+        let appearance = UITabBarAppearance()
+
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor = .clear
+        appearance.backgroundEffect = nil
+        appearance.shadowColor = .clear
+
+        configureTabBarItemAppearance(
+            appearance.stackedLayoutAppearance,
+            selectedColor: selectedColor,
+            normalColor: normalColor
+        )
+        configureTabBarItemAppearance(
+            appearance.inlineLayoutAppearance,
+            selectedColor: selectedColor,
+            normalColor: normalColor
+        )
+        configureTabBarItemAppearance(
+            appearance.compactInlineLayoutAppearance,
+            selectedColor: selectedColor,
+            normalColor: normalColor
+        )
+
+        let tabBar = UITabBar.appearance()
+        tabBar.tintColor = selectedColor
+        tabBar.unselectedItemTintColor = normalColor
+        tabBar.isTranslucent = true
+        tabBar.backgroundColor = .clear
+        tabBar.standardAppearance = appearance
+        tabBar.scrollEdgeAppearance = appearance
+    }
+
+    private func configureTabBarItemAppearance(
+        _ appearance: UITabBarItemAppearance,
+        selectedColor: UIColor?,
+        normalColor: UIColor?
+    ) {
+        appearance.selected.iconColor = selectedColor
+        appearance.normal.iconColor = normalColor
     }
 }
