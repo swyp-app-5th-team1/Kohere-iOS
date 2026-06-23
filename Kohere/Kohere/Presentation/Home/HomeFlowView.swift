b2a1f35c724b4c9fdd84658375172f510a8397a4
sumchive <haon0319@naver.com>
2026-06-24 02:37:18 +0900

    feat: HomeView 구현

--- Kohere/Kohere/Presentation/Home/HomeFlowView.swift
+++ Kohere/Kohere/Presentation/Home/HomeFlowView.swift
@@ -9,8 +9,13 @@ import ComposableArchitecture
 import SwiftUI
 
 struct HomeFlowView: View {
+    
+    // MARK: - Property
+    
     @Bindable var store: StoreOf<HomeFeature>
 
+    // MARK: - Body
+    
     var body: some View {
         NavigationStack(
             path: $store.scope(
@@ -19,8 +24,20 @@ struct HomeFlowView: View {
             )
         ) {
             HomeView(store: store)
-        } destination: { _ in
-            EmptyView()
+        } destination: { store in
+            switch store.case {
+            case let .savedListings(savedListingsStore):
+                SavedListingsView(store: savedListingsStore)
+                    .navigationBarHidden(true)
+                
+            case let .recentlyViewedList(recentlyViewedStore):
+                RecentlyViewedListngsView(store: recentlyViewedStore)
+                    .navigationBarHidden(true)
+            
+            case let .notifications(notificationsStore):
+                NotificationsView(store: notificationsStore)
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
