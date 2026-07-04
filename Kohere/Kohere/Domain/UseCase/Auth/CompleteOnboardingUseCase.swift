41d5b7517fba1e7e48c2667876ddaa0913a7cb8d
sumchive <haon0319@naver.com>
2026-07-05 00:31:47 +0900

    feat: 인증 온보딩 UseCase 추가

---
+++ Kohere/Kohere/Domain/UseCase/Auth/CompleteOnboardingUseCase.swift
@@ -0,0 +1,30 @@
+//
+//  CompleteOnboardingUseCase.swift
+//  Kohere
+//
+//  Created by mandoo on 7/3/26.
+//
+
+import ComposableArchitecture
+
+struct CompleteOnboardingUseCase {
+    var execute: (_ profile: AuthOnboardingProfile) async throws -> Auth
+}
+
+extension CompleteOnboardingUseCase: DependencyKey {
+    static let liveValue: CompleteOnboardingUseCase = {
+        @Dependency(\.authClient)
+        var authClient
+
+        return CompleteOnboardingUseCase { profile in
+            try await authClient.completeOnboarding(profile)
+        }
+    }()
+}
+
+extension DependencyValues {
+    var completeOnboardingUseCase: CompleteOnboardingUseCase {
+        get { self[CompleteOnboardingUseCase.self] }
+        set { self[CompleteOnboardingUseCase.self] = newValue }
+    }
+}
