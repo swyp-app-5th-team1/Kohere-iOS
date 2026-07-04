f5ab202f24187be7449fbe07b7c4cdc037b89ef7
sumchive <haon0319@naver.com>
2026-07-05 02:49:27 +0900

    chore: Foundation import

--- Kohere/Kohere/Presentation/Onboarding/DataError+.swift
+++ Kohere/Kohere/Presentation/Onboarding/DataError+.swift
@@ -5,6 +5,8 @@
 //  Created by mandoo on 7/5/26.
 //
 
+import Foundation
+
 extension DataError {
     static func from(_ error: Error) -> DataError {
         if let dataError = error as? DataError { return dataError }
