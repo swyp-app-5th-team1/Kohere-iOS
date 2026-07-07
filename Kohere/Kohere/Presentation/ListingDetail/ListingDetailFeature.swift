53743dccde2e32ec52586e29085c96a09a0d77ff
sumchive <haon0319@naver.com>
2026-07-07 17:24:52 +0900

    feat: 매물 찜 버튼 상태 연동

--- Kohere/Kohere/Presentation/ListingDetail/ListingDetailFeature.swift
+++ Kohere/Kohere/Presentation/ListingDetail/ListingDetailFeature.swift
@@ -6,12 +6,18 @@
 //
 
 import ComposableArchitecture
+import Foundation
 
 @Reducer
 struct ListingDetailFeature {
+    @Dependency(\.listingClient)
+    var listingClient
+
     @ObservableState
     struct State: Equatable {
         var detail: ListingDetailModel
+        var isFavoriteUpdating = false
+        var errorMessage: String?
 
         init(listingID: String) {
             self.detail = ListingDetailModel.mock(id: listingID)
@@ -21,6 +27,7 @@ struct ListingDetailFeature {
     enum Action: Equatable {
         case backButtonTapped
         case likeButtonTapped
+        case favoriteStatusResponse(Result<ListingFavoriteStatus, DataError>)
         case shareButtonTapped
         case contactButtonTapped
         case applyButtonTapped
@@ -30,7 +37,35 @@ struct ListingDetailFeature {
         Reduce { state, action in
             switch action {
             case .likeButtonTapped:
-                state.detail.overview.isLiked.toggle()
+                guard !state.isFavoriteUpdating else { return .none }
+
+                state.isFavoriteUpdating = true
+                state.errorMessage = nil
+
+                return .run { [listingClient, listingID = state.detail.id, isLiked = state.detail.overview.isLiked] send in
+                    do {
+                        let status: ListingFavoriteStatus
+                        if isLiked {
+                            status = try await listingClient.removeFavorite(listingID)
+                        } else {
+                            status = try await listingClient.addFavorite(listingID)
+                        }
+                        await send(.favoriteStatusResponse(.success(status)))
+                    } catch {
+                        await send(.favoriteStatusResponse(.failure(.from(error))))
+                    }
+                }
+
+            case let .favoriteStatusResponse(.success(status)):
+                state.detail.overview.isLiked = status.isFavorited
+                state.detail.overview.favoriteCount = status.favoriteCount
+                state.isFavoriteUpdating = false
+                state.errorMessage = nil
+                return .none
+
+            case let .favoriteStatusResponse(.failure(error)):
+                state.isFavoriteUpdating = false
+                state.errorMessage = error.localizedDescription
                 return .none
 
             case .backButtonTapped, .shareButtonTapped, .contactButtonTapped, .applyButtonTapped:
