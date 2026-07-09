119b6e24bac9ecfb541e8a6a53874c32d12fd836
SongKyuSeob <sks5075@naver.com>
2026-07-09 15:21:04 +0900

    fix: 검색 화면 종료 경고와 린트 정리

--- Kohere/Kohere/Presentation/Search/SearchView.swift
+++ Kohere/Kohere/Presentation/Search/SearchView.swift
@@ -27,6 +27,9 @@ struct SearchView: View {
         .onAppear {
             isSearchFocused = true
         }
+        .onDisappear {
+            isSearchFocused = false
+        }
     }
 
     private var searchHeader: some View {
@@ -61,7 +64,10 @@ struct SearchView: View {
                 "지역, 학교, 지하철역",
                 text: Binding(
                     get: { store.searchText },
-                    set: { store.send(.searchTextChanged($0)) }
+                    set: { text in
+                        guard store.searchText != text else { return }
+                        store.send(.searchTextChanged(text))
+                    }
                 )
             )
                 .kohereTextStyle(.label1Medium)
@@ -240,6 +246,7 @@ struct SearchView: View {
 
     private func placeResultRow(_ placeResult: SearchPlaceResult) -> some View {
         Button {
+            isSearchFocused = false
             store.send(.placeResultTapped(placeResult))
         } label: {
             HStack(alignment: .top, spacing: 8) {
